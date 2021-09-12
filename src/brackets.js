fb1795d67c263b7157784fc444428ca8639c7d47
Dylan Harville <dr.dyloh@gmail.com>
2021-09-11 22:06:36 -0600

    brackets tests passing, but I am not crazy about my solution. Maybe try solving with open/close key/value pairs

--- src/brackets.js
+++ src/brackets.js
@@ -1,5 +1,63 @@
 const Stack = require("./lib/stack");
 
-const match = (expression) => {};
+const match = (expression) => {
+    const newStack = new Stack();
+
+    for (let index = 0, limit = expression.length; index < limit; index++) {
+        switch (expression[index]) {
+            case '(':
+            case '[':
+            case '{':
+                newStack.push(expression[index]);
+                break;
+            case ')':
+                if (newStack.top ?.value === '(') {
+                    newStack.pop();
+                } else return false;
+                break;
+            case ']':
+                if (newStack.top ?.value === '[') {
+                    newStack.pop();
+                } else return false
+                break;
+            case '}':
+                if (newStack.top ?.value === '{') {
+                    newStack.pop();
+                } else return false;
+        }
+    }
+   return !newStack.top;
+};
+
+
 
 module.exports = match;
+
+// const Stack = require("./lib/stack");
+
+// const match = (expression) => {
+//     const newStack = new Stack();
+//     expression = expression.replace(/\s/g, '');
+
+//     expression.split('').forEach(character => { 
+//         if ('([{'.includes(character)) {
+//             newStack.push(character);
+//         } else {
+//             if (')]}'.includes(character)) {
+//                 if (newStack.top) {
+//                     (newStack.top ?.value === '(' && character === ')') ?
+//                         newStack.pop() : null;
+//                     (newStack.top ?.value === '[' && character === ']') ?
+//                         newStack.pop() : null;
+//                     (newStack.top ?.value === '{' && character === '}') ?
+//                         newStack.pop() : null;
+//                 }
+//             } else {
+//                 return false;
+//             }
+//         }
+//     });
+//    return !newStack.top;
+// };
+
+// module.exports = match;
